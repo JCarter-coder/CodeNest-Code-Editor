5d3a21d1d7eb536fb62e7087736d0979a467272c
JCarter-coder <jessetcarter@hotmail.com>
2025-08-03 17:12:15 +0000

    File new, open, and save functioning properly

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -37,7 +37,13 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBoxEditor = new System.Windows.Forms.RichTextBox();
+            this.statusStripMainStatus = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelMain = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialogMain = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogMain = new System.Windows.Forms.SaveFileDialog();
             this.menuStripMainMenus.SuspendLayout();
+            this.statusStripMainStatus.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStripMainMenus
@@ -47,7 +53,7 @@
             this.editToolStripMenuItem});
             this.menuStripMainMenus.Location = new System.Drawing.Point(0, 0);
             this.menuStripMainMenus.Name = "menuStripMainMenus";
-            this.menuStripMainMenus.Size = new System.Drawing.Size(800, 24);
+            this.menuStripMainMenus.Size = new System.Drawing.Size(1184, 24);
             this.menuStripMainMenus.TabIndex = 0;
             this.menuStripMainMenus.Text = "Menus";
             // 
@@ -68,30 +74,35 @@
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
             this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
@@ -106,18 +117,53 @@
             this.findToolStripMenuItem.Name = "findToolStripMenuItem";
             this.findToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.findToolStripMenuItem.Text = "Find";
+            this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
+            // 
+            // richTextBoxEditor
+            // 
+            this.richTextBoxEditor.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBoxEditor.Location = new System.Drawing.Point(0, 51);
+            this.richTextBoxEditor.Name = "richTextBoxEditor";
+            this.richTextBoxEditor.Size = new System.Drawing.Size(1184, 493);
+            this.richTextBoxEditor.TabIndex = 1;
+            this.richTextBoxEditor.Text = "";
+            // 
+            // statusStripMainStatus
+            // 
+            this.statusStripMainStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelMain});
+            this.statusStripMainStatus.Location = new System.Drawing.Point(0, 29);
+            this.statusStripMainStatus.Name = "statusStripMainStatus";
+            this.statusStripMainStatus.Size = new System.Drawing.Size(1184, 22);
+            this.statusStripMainStatus.TabIndex = 2;
+            this.statusStripMainStatus.Text = "statusStripMain";
+            // 
+            // toolStripStatusLabelMain
+            // 
+            this.toolStripStatusLabelMain.Name = "toolStripStatusLabelMain";
+            this.toolStripStatusLabelMain.Size = new System.Drawing.Size(139, 17);
+            this.toolStripStatusLabelMain.Text = "toolStripStatusLabelMain";
+            // 
+            // openFileDialogMain
+            // 
+            this.openFileDialogMain.FileName = "openFileDialogMain";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 544);
+            this.Controls.Add(this.statusStripMainStatus);
+            this.Controls.Add(this.richTextBoxEditor);
             this.Controls.Add(this.menuStripMainMenus);
             this.MainMenuStrip = this.menuStripMainMenus;
             this.Name = "FormMain";
             this.Text = "CodeNest";
+            this.Load += new System.EventHandler(this.FormMain_Load);
             this.menuStripMainMenus.ResumeLayout(false);
             this.menuStripMainMenus.PerformLayout();
+            this.statusStripMainStatus.ResumeLayout(false);
+            this.statusStripMainStatus.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,6 +180,11 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBoxEditor;
+        private System.Windows.Forms.StatusStrip statusStripMainStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelMain;
+        private System.Windows.Forms.OpenFileDialog openFileDialogMain;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogMain;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -7,14 +7,180 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CodeNest_Code_Editor
 {
     public partial class FormMain : Form
     {
+        // Let's create a member of our class variable to keep track of the current file path
+        private string m_currentFilePath = string.Empty;
         public FormMain()
         {
+            // This will run the Visual Studio code that VS generated/wrote.
             InitializeComponent();
+
+            // Let's set up our custom styling after the form is created
+            SetupStyling();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // This is our code to set up our custom styling after the form has been created
+        private void SetupStyling()
+        {
+            // Let's give our form a darker theme for now
+            this.BackColor = Color.DarkBlue;
+            //this.ForeColor = Color.FromArgb(200, 200, 200);
+            // Let's style the menu strip
+            menuStripMainMenus.BackColor = Color.DarkGreen;
+            menuStripMainMenus.ForeColor = Color.White;
+
+            // Let's style the editor which is our rich text box
+            richTextBoxEditor.BackColor = Color.Black;
+            richTextBoxEditor.ForeColor = Color.White;
+            richTextBoxEditor.Font = new Font("Consolas", 12);
+            richTextBoxEditor.AcceptsTab = true;
+            richTextBoxEditor.WordWrap = true;
+
+            // Let's style the status strip
+            statusStripMainStatus.BackColor = Color.Purple;
+            toolStripStatusLabelMain.BackColor = Color.White;
+            toolStripStatusLabelMain.Text = "Type your code in here...";
+
+            // Let's expand the window title to set it to this
+            // When we type in the word this, it means the code behind for the form we are in
+            this.Text = "CodeNest Code Editor - Untitled";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // The user raised an event to start a new file
+            // As developers, we need to handle this event
+            // First let's clear all the text in the editor
+            richTextBoxEditor.Clear();
+            // Clear our current file path
+            m_currentFilePath = string.Empty;
+            // Now let's set the current title 
+            this.Text = "CodeNest Code Editor - Untitled";
+            // Let the user know what is going on
+            toolStripStatusLabelMain.Text = "New file created";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Let's close the application at the user's request
+            // The user raised the event to close the application
+            // Here we handle the close event with our code
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This method will run when the user raises this click event by clicing on the open from our file menu
+            // Let's give our user a dialog to choose a file to open
+            if (openFileDialogMain.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Now let's try to read the user selected file and put it in the editor
+                    string fileContent = File.ReadAllText(openFileDialogMain.FileName);
+                    richTextBoxEditor.Text = fileContent;
+                    m_currentFilePath = openFileDialogMain.FileName;
+                    this.Text = "CodeNest Code Editor - " + Path.GetFileName(openFileDialogMain.FileName);
+                    toolStripStatusLabelMain.Text = "Opened file: " + Path.GetFileName(openFileDialogMain.FileName);
+                }
+                catch (Exception ex)
+                {
+                    // If trying to open an existing file fails, an event is raised and we handle it here
+                    MessageBox.Show("Error opening file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This method will run when the user raises this click event by clicing on the save from our file menu
+            // Now let's check where we can save the file, do we already have a file path from a previous save?
+            if (string.IsNullOrEmpty(m_currentFilePath))
+            {
+                // Show a dialog to chosse where to save the current file
+                if (saveFileDialogMain.ShowDialog() == DialogResult.OK)
+                {
+                    m_currentFilePath = saveFileDialogMain.FileName;
+                }
+                else
+                {
+                    // If we are here, the user cancelled the save
+                    // Do nothing
+                    return;
+                }
+            }
+
+            // Let's try to save the text in the editor to the file path
+            try
+            {
+                File.WriteAllText(m_currentFilePath, richTextBoxEditor.Text);
+                this.Text = "CodeNest Code Editor - " + Path.GetFileName(m_currentFilePath);
+                toolStripStatusLabelMain.Text = "Saved file: " + Path.GetFileName(m_currentFilePath);
+            }
+            catch (Exception ex)
+            {
+                // Show an error box to the user if the file cannot be saved
+                MessageBox.Show("Error saving file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Debug Save As method
+            // This method will run when the user raises this click event by clicing on the save as from our file menu
+            // Now let's show the user the save dialog box even if we already have another current file
+            // in our editor that we could just press save to save
+            try
+            {
+                // Give user option to save the text file in a new or the same location
+                File.WriteAllText(saveFileDialogMain.FileName, richTextBoxEditor.Text);
+                m_currentFilePath = saveFileDialogMain.FileName;
+                this.Text = "CodeNest Code Editor - " + Path.GetFileName(m_currentFilePath);
+                toolStripStatusLabelMain.Text = "Saved file as: " + Path.GetFileName(m_currentFilePath);
+            }
+            catch (Exception ex)
+            {
+                // If an error is raised, show it to the user via message box
+                MessageBox.Show("Error saving as file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //// First let's ask our user what text are you looking for?
+            //string searchText = Microsoft.VisualBasic.Interaction.InputBox("Enter the text to find:", "Find", "");
+
+            //// This checks if the user entred something to search for
+            //if (!string.IsNullOrEmpty(searchText))
+            //{
+            //    // This searches for the text in the rich text box
+            //    int index = richTextBoxEditor.Find(searchText, RichTextBoxFinds.None);
+
+            //    if (index >= 0)
+            //    {
+            //        // Let's highlight the found text
+            //        richTextBoxEditor.SelectionStart = index;
+            //        richTextBoxEditor.SelectionLength = searchText.Length;
+            //        richTextBoxEditor.Focus();
+            //        richTextBoxEditor.Text = "Found: " + searchText;
+            //    }
+            //    else
+            //    {
+            //        // This tells the user if the text wasn't found
+            //        toolStripStatusLabelMain.Text = "Text not found: " + searchText;
+            //        MessageBox.Show("Text not found!", "Find", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    }
+            //}
         }
     }
 }
